f9c699f7f993d542c0ace2f1e582d98a20ddc178
AlbanaCelepija <acelepija@fbk.eu>
2026-09-23 14:48:44 +0200

    fix(gui)provide correct editable module in reqs

--- gui/utils.py
+++ gui/utils.py
@@ -24,7 +24,7 @@ USE_CASES_FOLDER = os.path.join(parent_folder, "framework/temlops/use_cases")
 # Path to the fairops ontology (lives in the separate fairness_ontology/fairops
 # project). Override with the FAIROPS_ONTOLOGY_PATH env var if it's located
 # elsewhere on disk.
-FAIROPS_ONTOLOGY_PATH = "./fairops/fairops.ttl"
+FAIROPS_ONTOLOGY_PATH = "fairops/fairops.ttl"
 #os.environ.get(
 #    "FAIROPS_ONTOLOGY_PATH",
 #)
